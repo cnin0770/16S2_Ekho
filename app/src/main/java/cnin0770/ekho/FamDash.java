114d6eac890814a727f3bb8f1aac92d4d4a9564c
cnin0770 <100277977@qq.com>
2016-10-14 09:54:25 +1100

    firstUpload

---
+++ app/src/main/java/cnin0770/ekho/FamDash.java
@@ -0,0 +1,7 @@
+package cnin0770.ekho;
+
+/**
+ * Created by cnin0770 on 7/10/16.
+ */
+public class FamDash {
+}
---
+++ app/src/main/java/cnin0770/ekho/Processor.java
@@ -0,0 +1,13 @@
+package cnin0770.ekho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Processor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_processor);
+    }
+}
